ffdb601a0a02fab088abfcb83d72be17eafd4781
Alexandru Negrila <alex-codes@arntech.ro>
2020-08-18 12:22:43 +0000

    Fix extra modules

--- conf/enabled_modules.sh
+++ conf/enabled_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #INSP_MODULES_ENABLED="${INSP_LINKS_COUNT:-0}"
-for module in ${INSP_MODULES_ENABLED[@]}; do
+for module in ${INSP_MODULES_ENABLED}; do
 	echo "<module name=\"${module}.so\">"
 done
 
